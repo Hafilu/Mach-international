a38539b0e5364bd80820927bf483282d9d7ab594
Hafilue c <hafiluecmuble@gmail.com>
2024-12-10 14:49:03 +0530

    v3 with change in company culture and map added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import banner1 from "../assets/banner1.jpg";
 import banner2 from "../assets/banner2.jpg";
 import banner3 from "../assets/banner3.jpg";
-import missionbg from "../assets/missionbg.jpg";
-import insta from "../assets/insta.png";
-import linkdin from "../assets/linkdin.png";
-import x from "../assets/twitter.png";
-import fb from "../assets/fb.png";
+import mission from "../assets/mission.png";
+import vission from "../assets/vission.png";
+import value from "../assets/value.png";
+import map from "../assets/map.png";
 import core from "../assets/core.jpg";
-import corebg from "../assets/business.jpg"
-import about from "../assets/about.jpg";
+import corebg from "../assets/business.jpg";
+
 import stand from "../assets/standout.jpg";
 import inspection from "../assets/inspection.jpg";
 import construction from "../assets/construction.jpg";
@@ -22,9 +21,6 @@ import ServiceCard from "../Components/ServiceCard";
 import ContactSection from "../Components/Contact";
 import AddressCard from "../Components/AddressCard";
 import Footer from "../Components/Footer";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const banners = [
   {
@@ -58,7 +54,7 @@ const Ourvalues = [
   { title: "Sustainability", text: "Duis aute irure dolor in." },
   { title: "Empowerment", text: "Duis aute irure dolor in." },
   { title: "Integrity", text: "Duis aute irure dolor in." },
- 
+
   { title: "Accountability", text: "Duis aute irure dolor in." },
   { title: "Transparency", text: "Duis aute irure dolor in." },
 ];
@@ -224,9 +220,9 @@ const Home = () => {
                 <img
                   src="https://img.freepik.com/free-photo/construction-site-silhouettes_1127-3253.jpg?t=st=1733738896~exp=1733742496~hmac=ecb0df6018f757857eb00a4fc20443ef8b32a9ebb27e2208a4eb730bf6493c66&w=900"
                   alt="Business Representation"
-                  className="w-full h-[600px] object-cover rounded-lg shadow-lg "
+                  className="w-full h-[550px] object-cover rounded-lg shadow-lg "
                 />
-                
+
                 <div className="mt-12  font-figtree w-[80%]  grid grid-cols-6 md:grid-cols-2 absolute bottom-[50px] left-1/2 transform -translate-x-1/2 gap-6   text-center">
                   {/* Statistic Item */}
                   <div className="bg-white bg-opacity-20 backdrop-blur-md  rounded-lg p-5">
@@ -263,133 +259,108 @@ const Home = () => {
         </div>
       </section>
 
-      <div
-        className="py-24 relative"
-        style={{
-          backgroundImage: `url(${missionbg})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        {/* Black Overlay */}
-        <div className="absolute inset-0 bg-black bg-opacity-10"></div>
-        <div className="relative z-10  ">
-          <div className="  bg-cover bg-center flex items-center justify-center">
-            <div className="w-[85%] mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 ">
-              {/* Left Section */}
-              <div className="md:w-[70%]  ">
-                <Slider {...sliderSettings}>
-                  <div className="px-3">
-                    <div className="bg-gray-800  p-10   text-white font-playfair rounded-lg shadow-md">
-                      <div>
-                        <h2 className="text-3xl font-bold  uppercase mb-4">
-                          Our Mission
-                        </h2>
-                        <p className="text-lg">
-                          Lorem ipsum dolor, sit amet consectetur adipisicing
-                          elit. Quas dolor illo quia sed quisquam sint accusamus
-                          fugit deserunt harum quis impedit est, voluptatibus
-                          
-                        </p>
-                        <p className="mt-2 mb-4 text-lg">
-                          Lorem ipsum dolor, sit amet consectetur adipisicing
-                          elit. Quas dolor illo quia sed quisquam sint accusamus
-                           
-                        </p>
+      <section className=" w-[85%] mx-auto py-24">
+        <h2 className=" text-4xl font-bold font-playfair text-center mx-auto md:w-[50%] mb-16">
+          Our Company Culture
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3  ">
+          <div className="flex justify-center flex-col items-center pt-3   hover:bg-[#eaf3ff] hover:shadow-lg transition duration-300">
+            <img
+              src={mission}
+              alt=""
+              className="w-[80px] h-[80px] object-cover"
+            />
 
-                         
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="px-3">
-                    <div className="bg-gray-800  p-10  font-playfair text-white rounded-lg shadow-md">
-                      <div>
-                        <h2 className="text-3xl font-bold  uppercase mb-4">
-                          Our vission
-                        </h2>
-                        <p className="text-lg">
-                          Lorem ipsum dolor, sit amet consectetur adipisicing
-                          elit. Quas dolor illo quia sed quisquam sint accusamus
-                          fugit deserunt harum quis impedit est, voluptatibus
-                           
-                        </p>
-                        <p className="mt-2 mb-4 text-lg">
-                          Lorem ipsum dolor, sit amet consectetur adipisicing
-                          elit. Quas dolor illo quia sed quisquam sint accusamus
-                         
-                        </p>
-
-                        
-                      </div>
-                    </div>
-                  </div>
-                  <div className="px-3">
-                    <div className="bg-gray-800  p-10  font-playfair text-white rounded-lg shadow-md">
-                      <div>
-                        <h2 className="text-3xl font-bold  uppercase mb-4">
-                          Our values
-                        </h2>
-                        <p className="text-lg">
-                          Lorem ipsum dolor, sit amet consectetur adipisicing
-                          elit. Quas dolor illo quia sed quisquam sint accusamus
-                          fugit deserunt harum quis impedit est, voluptatibus
-                          
-                        </p>
-                        <p className="mt-2 mb-4 text-lg">
-                          Lorem ipsum dolor, sit amet consectetur adipisicing
-                          elit. Quas dolor illo quia sed quisquam sint accusamus
-                           
-                        </p>
-
-                         
-                      </div>
-                    </div>
-                  </div>
-                </Slider>
-              </div>
-
-              {/* Right Section */}
-              <div className="flex items-end justify-end flex-col  font-playfair text-white p-6 rounded-lg shadow-md">
-                <p className="text-5xl font-medium text-center">
-                  The Future Is Here
-                </p>
-                <p className="text-right text-lg py-10">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Dolore, laudantium delectus veritatis nemo magnam labore
-                  asperiores! Modi ipsum dolores cupiditate unde.{" "}
-                </p>
-                <div className="flex space-x-4">
-                  <a
-                    href="#"
-                    className="   flex items-center justify-center w-[30px] h-[30px] rounded-full bg-white  text-[#00235a]  "
-                  >
-                    <img src={fb} alt="" />
-                  </a>
-                  <a
-                    href="#"
-                    className="  p-2  flex items-center justify-center  w-[30px] h-[30px] rounded-full bg-white   text-[#00235a]  "
-                  >
-                    <img src={insta} alt="" />
-                  </a>
-                  <a
-                    href="#"
-                    className="   p-2  flex items-center justify-center   w-[30px] h-[30px] rounded-full bg-white  text-[#00235a]  "
-                  >
-                    <img src={x} alt="" />
-                  </a>
-                  <a
-                    href="#"
-                    className="   p-2 flex items-center justify-center  w-[30px] h-[30px] rounded-full bg-white   text-[#00235a]  "
-                  >
-                    <img src={linkdin} alt="" />
-                  </a>
-                </div>
+            <h2 className="text-2xl font-bold font-figtree mt-6 ">
+              OUR <span className="text-[#104cba]">MISSION</span>
+            </h2>
+            <div className="flex items-center justify-center  py-6 ">
+              <div className="flex items-center space-x-1">
+                <div className="border-t border-[#104cba] w-20"></div>
+                <span className="text-[#104cba] text-lg">★</span>
+                <div className="border-t border-[#104cba] w-20"></div>
               </div>
             </div>
+
+            <img
+              src={mission}
+              alt=""
+              className="w-[30px] h-[30px] object-cover"
+            />
+            <p className="text-lg mb-2 text-center font-playfair text-gray-800 px-16 py-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+              ratione sed dolor voluptatibus! Quos veniam assumenda illo
+              voluptate necessitatibus tenetur, beatae mollitia dolores id
+              aliquam veritatis odit cum optio modi!
+            </p>
+          </div>
+
+          <div className="flex justify-center flex-col items-center pt-3   hover:bg-[#eaf3ff] hover:shadow-lg transition duration-300">
+            <div className="w-full flex justify-center flex-col items-center border-x-2 border-gray-500">
+              <img
+                src={vission}
+                alt=""
+                className="w-[90px] h-[90px] object-cover "
+              />
+
+              <h2 className="text-2xl font-bold font-figtree mt-6 ">
+                OUR <span className="text-[#104cba]">VISSION</span>
+              </h2>
+            </div>
+            <div className="flex items-center justify-center  py-6 ">
+              <div className="flex items-center space-x-1">
+                <div className="border-t border-[#104cba] w-20"></div>
+                <span className="text-[#104cba] text-lg">★</span>
+                <div className="border-t border-[#104cba] w-20"></div>
+              </div>
+            </div>
+            <div className="w-full flex justify-center flex-col items-center border-x-2 border-gray-500">
+              <img
+                src={vission}
+                alt=""
+                className="w-[30px] h-[30px] object-cover"
+              />
+            </div>
+            <p className="text-lg mb-2 text-center font-playfair text-gray-800 px-16 py-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+              ratione sed dolor voluptatibus! Quos veniam assumenda illo
+              voluptate necessitatibus tenetur, beatae mollitia dolores id
+              aliquam veritatis odit cum optio modi!
+            </p>
+          </div>
+
+          <div className="flex justify-center flex-col items-center pt-3   hover:bg-[#eaf3ff] hover:shadow-lg transition duration-300">
+            <img
+              src={value}
+              alt=""
+              className="w-[90px] h-[90px] object-cover"
+            />
+
+            <h2 className="text-2xl font-bold font-figtree mt-6 ">
+              OUR <span className="text-[#104cba]">VALUES</span>
+            </h2>
+            <div className="flex items-center justify-center  py-6 ">
+              <div className="flex items-center space-x-1">
+                <div className="border-t border-[#104cba] w-20"></div>
+                <span className="text-[#104cba] text-lg">★</span>
+                <div className="border-t border-[#104cba] w-20"></div>
+              </div>
+            </div>
+
+            <img
+              src={value}
+              alt=""
+              className="w-[30px] h-[30px] object-cover"
+            />
+            <p className="text-lg mb-2 text-center font-playfair text-gray-800 px-16 py-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+              ratione sed dolor voluptatibus! Quos veniam assumenda illo
+              voluptate necessitatibus tenetur, beatae mollitia dolores id
+              aliquam veritatis odit cum optio modi!
+            </p>
           </div>
         </div>
-      </div>
+      </section>
 
       <section className="bg-gray-100 py-24">
         {" "}
@@ -451,11 +422,14 @@ const Home = () => {
       <section className="pb-24 bg-gray-100" id="core-value">
         <div className="relative">
           <img src={core} alt="" className="w-full h-[450px]  object-cover" />{" "}
-          <div className="w-[75%] bg-white rounded-lg shadow-md absolute bottom-[-150px] left-1/2 transform -translate-x-1/2 z-10"  style={{
-          backgroundImage: `url(${corebg})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}>
+          <div
+            className="w-[75%] bg-white rounded-lg shadow-md absolute bottom-[-150px] left-1/2 transform -translate-x-1/2 z-10"
+            style={{
+              backgroundImage: `url(${corebg})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
             <h2 className=" text-4xl font-bold font-playfair text-center   py-10">
               Our Core Values
             </h2>
@@ -471,23 +445,23 @@ const Home = () => {
           </div>
         </div>
         <div className="w-[85%] mx-auto mt-[200px]">
-         
-            <div className="grid grid-cols-1 md:grid-cols-2    w-[70%] mx-auto gap-y-10 gap-x-20   ">
-              {Ourvalues.map((value, index) => (
-                <div key={index} className="flex items-start">
-                  {/* Icon */}
-                  <div className="text-2xl text-[#104cba] mr-4">➤</div>
-                  {/* Content */}
-                  <div>
-                    <h3 className="text-2xl font-playfair font-bold mb-2">
-                      {value.title}
-                    </h3>
-                    <p className="text-lg font-playfair text-gray-800">{value.text}</p>
-                  </div>
+          <div className="grid grid-cols-1 md:grid-cols-2    w-[70%] mx-auto gap-y-10 gap-x-20   ">
+            {Ourvalues.map((value, index) => (
+              <div key={index} className="flex items-start">
+                {/* Icon */}
+                <div className="text-2xl text-[#104cba] mr-4">➤</div>
+                {/* Content */}
+                <div>
+                  <h3 className="text-2xl font-playfair font-bold mb-2">
+                    {value.title}
+                  </h3>
+                  <p className="text-lg font-playfair text-gray-800">
+                    {value.text}
+                  </p>
                 </div>
-              ))}
-            </div>
-         
+              </div>
+            ))}
+          </div>
         </div>
       </section>
 
@@ -495,10 +469,14 @@ const Home = () => {
         <ContactSection />
       </section>
 
-      <section className="w-[85%] mx-auto py-24">
-        <h2 className=" text-4xl font-bold font-playfair text-center mx-auto md:w-[60%] mb-16">
+      <section className="w-[85%] mx-auto pt-20  ">
+        <h2 className=" text-4xl font-bold font-playfair text-center mx-auto md:w-[60%] ">
           Our Branches
         </h2>
+        <img src={map} alt="" className="w-full   object-cover" />
+      </section>
+
+      <section className="w-[85%] mx-auto pb-24">
         <AddressCard addresses={addresses} />
       </section>
 
