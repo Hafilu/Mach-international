9d89a3189df9b13aca55c79dd5cb08dce6668978
Hafilue c <hafiluecmuble@gmail.com>
2025-01-06 10:57:48 +0530

    scroll top added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Services from "./Pages/Service";
 import Footer from "./Components/Footer";
 import { useEffect, useState } from "react";
 import { fetchSiteData } from "./Api/Api";
+import ScrollToTop from "./Components/ScrollToTop";
 
 function App() {
   const [data, setData] = useState(null);
@@ -23,17 +24,18 @@ function App() {
     loadData();
   }, []);
 
- 
   return (
     <>
       {data?.menus && <Navbar menu={data?.menus} site_info={data?.site_info} />}
-
+      <ScrollToTop />
       {/*Implementing Routes for respective Path */}
       <Routes>
         <Route path="/" element={<Home footer_info={data} />} />
-        <Route path="/services/:serviceId" element={<Services  footer_info={data}/>} />
+        <Route
+          path="/services/:serviceId"
+          element={<Services footer_info={data} />}
+        />
       </Routes>
-      
     </>
   );
 }
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { HashLink as NavLink } from "react-router-hash-link";
 
 const Button = ({ to, text, color }) => {
   return (
     <div className="my-3">
-      <NavLink to={to}>
+      <NavLink   smooth to={to}>
         <span
           style={{ color: `${color && color}` }}
           className="border-white border py-3 px-8 rounded-md text-base font-medium transition-colors duration-300 ease-in-out transform hover:bg-[#104cba] hover:text-white hover:border-[#104cba]  active:opacity-70"
